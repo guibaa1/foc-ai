1e9a1ef40a7fe90917cc5929125e64caed74a20e
Guilherme Costa <gcsantossdev@gmail.com>
2026-05-20 09:45:11 -0300

    feat: adiciona repetição ao menu principal

--- src/main/java/br/com/desafio/Main.java
+++ src/main/java/br/com/desafio/Main.java
@@ -9,44 +9,55 @@ public class Main {
         int opcao = 0;
         Scanner read = new Scanner(System.in);
 
-        System.out.println("===== FOC.AI =====");
-        System.out.println(" 1 - Cadastrar tarefa");
-        System.out.println(" 2 - Listar tarefas");
-        System.out.println(" 3 - Concluir tarefa");
-        System.out.println(" 4 - Cadastrar meta");
-        System.out.println(" 5 - Ver progresso");
-        System.out.println(" 0 - Sair");
-        System.out.println(" ");
-        System.out.print(" Digite a opçao desejada: ");
+        do {
 
-        opcao = read.nextInt();
+            System.out.println("===== FOC.AI =====");
+            System.out.println(" 1 - Cadastrar tarefa");
+            System.out.println(" 2 - Listar tarefas");
+            System.out.println(" 3 - Concluir tarefa");
+            System.out.println(" 4 - Cadastrar meta");
+            System.out.println(" 5 - Ver progresso");
+            System.out.println(" 0 - Sair");
+            System.out.println(" ");
+            System.out.print(" Digite a opçao desejada: ");
 
-        switch (opcao){
+            opcao = read.nextInt();
 
-            case 0 :
-                System.out.println("Saindo do menu");
-                break;
+            System.out.println(" ");
 
-            case 1 :
-                System.out.println("Cadastrar tarefa selecionado !!");
-                break;
+            switch (opcao) {
 
-            case 2:
-                System.out.println("Listar tarefa selecionado !!");
-                break;
+                case 0:
+                    System.out.println("Saindo do menu");
+                    break;
 
-            case 3:
-                System.out.println("Concluir tarefa selecionado !!");
-                break;
+                case 1:
+                    System.out.println("Cadastrar tarefa selecionado !!");
+                    break;
 
-            case 4:
-                System.out.println("Cadastrar meta selecionado !!");
-                break;
+                case 2:
+                    System.out.println("Listar tarefa selecionado !!");
+                    break;
 
-            case 5:
-                System.out.println("Ver progresso selecionado !!");
-                break;
+                case 3:
+                    System.out.println("Concluir tarefa selecionado !!");
+                    break;
+
+                case 4:
+                    System.out.println("Cadastrar meta selecionado !!");
+                    break;
+
+                case 5:
+                    System.out.println("Ver progresso selecionado !!");
+                    break;
+
+                default:
+                    System.out.println("Digito inválido, tente novamente!!\n");
+                    break;
+            }
         }
+        while(opcao != 0);
+
 
         read.close();
     }
